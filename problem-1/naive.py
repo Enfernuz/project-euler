3594eb3fb56d7360e9a19f59d1ee687c04fa5fab
Arsentii Nerushev <asnerushev@gmail.com>
2019-04-08 23:26:09 +0300

    [fix] Reread the problem's description. The end number must be below 1000, not equal to.

--- problem-1/naive.py
+++ problem-1/naive.py
@@ -1,5 +1,5 @@
 sum = 0
-for x in range(1, 1001):
+for x in range(1, 1000):
   if x % 3 == 0 or x % 5 == 0:
     sum += x
 
--- problem-1/optimized.py
+++ problem-1/optimized.py
@@ -3,7 +3,7 @@ def sumOfMultipliers(end, x):
   n = end / x
   return (n * (n + 1) / 2) * x
 
-end = 1000
+end = 999 # below 1000
 x = 3
 y = 5
 # We have to subtract one sum of the multipliers of x * y because these multipliers take part in both of the sums of the multipliers of x and y and hence contribute twice in the final sum.
